216ea3c0d18781b3dd9735cd75ecc4f25f84452a
Gang jun seok <rkdwnstjr1925@naver.com>
2025-12-01 21:03:02 +0900

    feat : flutter_theme_example

--- flutter_theme_example/lib/home_page.dart
+++ flutter_theme_example/lib/home_page.dart
@@ -6,18 +6,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+
+      // ListView로 3개의 색상 박스를 표시
       body: ListView(
         children: [
+          /// AppThemeExtension.main 색상 적용
+          /// context.appColor → BuildContext extension을 통해
+          /// ThemeExtension(AppThemeExtension)을 직접 가져오는 방식
           Container(
             height: 100,
             width: double.infinity,
             color: context.appColor.main,
           ),
+
+          /// mainLight 색상 적용
           Container(
             height: 100,
             width: double.infinity,
             color: context.appColor.mainLight,
           ),
+
+          /// sub 색상 적용
           Container(
             height: 100,
             width: double.infinity,
